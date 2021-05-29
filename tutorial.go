390b534e9077e674cd63b5a9373b4412ddeba5ff
Rannie <einnar82@gmail.com>
2021-05-29 13:50:16 +0800

    String concatination

--- tutorial.go
+++ tutorial.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello world")
+	// concatinate string in go
+	var name string = "rannie"
+	result := fmt.Sprintf("%s %s", "Hello", name)
+	fmt.Println(result)
+	// another one
+	var lastname string = "again"
+	fmt.Println("Hello " + lastname)
 }

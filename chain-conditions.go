31170729504fbc56b2c40db210f6c2cdef925448
Rannie Ollit <rannie.ollit@petnet.com.ph>
2021-06-02 14:01:18 +0800

    Added ifs

---
+++ chain-conditions.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	val := 5 < 7 || 7 > 9
+	fmt.Printf("%t", val)
+	// ! not
+	// || or
+	// && and
+}
---
+++ if-statements.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 19
+
+	if age >= 18 {
+		fmt.Println("You can ride!")
+	} else {
+		fmt.Println("You cannot ride!")
+	}
+
+	// if, else, and else if
+}

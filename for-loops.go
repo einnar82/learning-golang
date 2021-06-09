dc4af58b51a214b62dd347e7be4a2c565214fa04
Rannie Ollit <rannie.ollit@petnet.com.ph>
2021-06-09 08:50:21 +0800

    For loop

---
+++ for-loops.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for loop
+	for a := 0; a < 10; a++ {
+		fmt.Printf("value of a: %d\n", a)
+	}
+
+	x := 0
+	for x < 5 {
+		fmt.Println("hey")
+		x++
+	}
+
+}

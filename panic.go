3cd094344e63a182b9dd657297172bda45f4d9ca
Rannie Ollit <rannie.ollit@petnet.com.ph>
2021-06-11 15:28:57 +0800

    Sample order of execution

--- panic.go
+++ panic.go
@@ -1,12 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+func panicker() {
+	fmt.Println("about to panic")
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Error", err)
+		}
+	}()
+	fmt.Println("done panicking")
+	panic("something happened here")
+	
+}
 
 func main() {
 
 	// order of execution
 	// defer, panic
-	defer fmt.Println("After")
-	fmt.Println("Before")
-	panic("Error")
+	// defer fmt.Println("After")
+	// fmt.Println("Before")
+	// panic("Error")
+	fmt.Println("Start")
+	panicker()
+	fmt.Println("end")
 }

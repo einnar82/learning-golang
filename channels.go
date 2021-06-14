34300ccbf75430245415810034fdd525cb5e16e9
Rannie Ollit <rannie.ollit@petnet.com.ph>
2021-06-14 16:08:29 +0800

    Channels

--- channels.go
+++ channels.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+var wg = sync.WaitGroup{}
+
+func main() {
+	ch := make(chan int)
+
+	wg.Add(2)
+	// receiving goroutine
+	go func(ch <-chan int) {
+		i := <-ch
+		fmt.Println("receive")
+		fmt.Println(i)
+		wg.Done()
+	}(ch)
+	// sending goroutine
+	go func(ch chan<- int) {	
+		ch <- 42
+		fmt.Println("send")
+		wg.Done()
+	}(ch)
+
+	wg.Wait()
 }

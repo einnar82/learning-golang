4ab511f0ec940cfd94f145da5edae0a721f7c8d8
Rannie Ollit <rannie.ollit@petnet.com.ph>
2021-06-14 10:11:13 +0800

    Sync object

---
+++ goroutine.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// mutex and waitgroup are partners for goroutines
+// waitgroup is for wait the sequence
+// mutex is to lock the sequence or syncronize the pattern
+var waitGroup = sync.WaitGroup{}
+var counter = 0
+var mutex = sync.RWMutex{}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		waitGroup.Add(2)
+		mutex.RLock()
+		go sayHello()
+		mutex.Lock()
+		go increment()
+	}
+	waitGroup.Wait()
+}
+
+func sayHello() {
+	fmt.Printf("Hello #%v\n", counter)
+	mutex.RUnlock()
+	waitGroup.Done()
+}
+
+func increment() {
+	counter++
+	mutex.Unlock()
+	waitGroup.Done()
+}
